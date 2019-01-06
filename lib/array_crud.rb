a06816477ff7e90f3490510eb74952e633295208
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-06 19:28:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  famous_cats = ["lil","bub", "grumpy cat", "Maru"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+                    famous_cats << "Mostafa" << "Maya"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+famous_cats<< ("Mostafa" , "Maya")                                            
 end
 
 def remove_element_from_start_of_array(array)
